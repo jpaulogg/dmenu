af23caa931947c9a710156b56f9e37c4cba5c2a1
João Paulo G. Garcia <joaopauloggarcia@gmail.com>
2021-12-02 21:21:55 -0300

    config.h: cores.

--- config.h
+++ config.h
@@ -13,7 +13,7 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static const char *colors[SchemeLast][2] = {
 	/*               fg         bg       */
 	[SchemeNorm] = { "#d7d7d7", "#333333" },
-	[SchemeSel]  = { "#3f3f3f", "#87d7af" },
+	[SchemeSel]  = { "#282828", "#87d7af" },
 	[SchemeOut]  = { "#292929", "#87afaf" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
